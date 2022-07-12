d1352f26216a7371abb54ec37df4ad39a57bb078
jsi19 <jonathan.si0619@gmail.com>
2022-07-12 12:31:17 -0700

    Completed part A of chapsnat app

--- App.js
+++ App.js
@@ -1,13 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
+  const [messages, setMessages] = useState([]);
+  const [placeholder, setPlaceholder] = useState("Hi dad!");
+
+  
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'hi dad!',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'Yes, I am mom',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://media-exp2.licdn.com/dms/image/C4E03AQHdRc4RjD-_-g/profile-displayphoto-shrink_800_800/0/1653578770530?e=1663200000&v=beta&t=4Uxgb_kxnuciDqZ2ovBD2R4KQr14-tw7tWUWy_qg-yM',
+        },
+        image: 'https://media-exp2.licdn.com/dms/image/C4E03AQHdRc4RjD-_-g/profile-displayphoto-shrink_800_800/0/1653578770530?e=1663200000&v=beta&t=4Uxgb_kxnuciDqZ2ovBD2R4KQr14-tw7tWUWy_qg-yM',
+      }
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+        name: 'Rex',
+        avatar: 'https://media-exp2.licdn.com/dms/image/C5603AQENeyOU219l6w/profile-displayphoto-shrink_800_800/0/1551594444630?e=1663200000&v=beta&t=dm10IEW97yyBMQjCwEM-7Zx0ZQFrBWrq9kmzUGyI6d8',
+      }}
+      showUserAvatar = {true}
+      alwaysShowSend = {true}
+      renderUsernameOnMessage = {true}
+      
+    />
   );
 }
 
