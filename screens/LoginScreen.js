f67be115f5146c960c112d4e80fd84580da9a6f3
jsi19 <jonathan.si0619@gmail.com>
2022-07-19 14:33:29 -0700

    Added step 6 of lab by implementing signup(5)/login(6) functionality into App.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,30 @@
 export default function LoginScreen({navigation}) {
+    //State Variables for user's email and password
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
 
+    //getAuth() actually authenticates the user, I think
+	const auth = getAuth();
+
+    //Very similar handleSubmit() function as signupScreen.js
+    //Instead, we have signIn with email/pass instead of creting new user
+	async function handleSubmit() {
+		console.log("handle submit envoked!!")
+
+		await signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			const user = userCredential.user; 
+		})
+        //Not using error code again
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+            console.log(errorCode, "<---- error code");
+            console.log(errorMessage, "<--- error message")
+		});
+	}
+
+    //return outputs a screen for login where user can input email/pass
 	return (
 		<>
 			<Text style={styles.bigBlue}>Login Here</Text>
@@ -21,10 +44,15 @@ export default function LoginScreen({navigation}) {
 				/>
 			</View>
 			<TouchableOpacity style={styles.loginBtn} onPress={() => {
-				{/* what do you think will go here? */}
+				handleSubmit();
 			}}>
 				<Text style={styles.loginText}>LOGIN</Text>
 			</TouchableOpacity>
+            <TouchableOpacity style={styles.redirectBtn} onPress={() => {
+                navigation.navigate("Signup")
+            }}>
+                <Text>Don't have an account? Sign up here</Text>
+            </TouchableOpacity>
 		</>
 	)
 }
